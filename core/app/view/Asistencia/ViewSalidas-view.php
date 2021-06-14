a7aa65823a976c5dc23a35a8bb7d063f7c5c5ea6
Edixon <payruz16@gmail.com>
2021-06-14 11:35:25 -0500

    .

--- core/app/view/Asistencia/ViewSalidas-view.php
+++ core/app/view/Asistencia/ViewSalidas-view.php
@@ -74,26 +74,7 @@
                                        <input type="hidden" name="view" value=<?php echo $_GET["view"]; ?>>
                                        <button class="btn btn-secondary mg-r-5 mg-b-10" onclick="return pregunta()"><a data-toggle="tooltip" data-placement="top" title="Eliminar"><i class="fa fa-trash"></i></a></button>
                                     </form>
-
-                                    <span class="btn btn-secondary dropdown-toggle  mg-r-5 mg-b-10" data-toggle="dropdown"></span>
-
-                                    <div class="dropdown-menu dropdown-menu-right">
-
-                                       <?php if ($pago) { ?>
-                                          <form action="index.php?action=Asistencia/OutAsistencia" method="post">
-                                             <input type="hidden" name="id" value="<?php echo $asistencia->id;  ?>">
-                                             <input type="hidden" name="view" value=<?php echo $_GET["view"]; ?>>
-                                             <button class="dropdown-item"><i class="fa fa-sign-out"></i> SALIDA</button>
-                                          </form>
-                                       <?php } else { ?>
-
-                                          <form action="index.php?action=Pago/AddPago" method="post">
-                                             <input type="hidden" name="id" value="<?php echo $asistencia->id;  ?>">
-                                             <input type="hidden" name="view" value=<?php echo $_GET["view"]; ?>>
-                                             <button class="dropdown-item"><i class="fa fa-money"></i> PAGO</button>
-                                          </form>
-                                       <?php } ?>
-                                    </div>
+                                    
                                  </div>
                               </td>
                            </tr>
