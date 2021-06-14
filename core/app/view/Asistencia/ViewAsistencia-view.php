32a08d2e7dc3b9295587e386275b303d7d18c565
Edixon <payruz16@gmail.com>
2021-06-14 11:14:41 -0500

    .

--- core/app/view/Asistencia/ViewAsistencia-view.php
+++ core/app/view/Asistencia/ViewAsistencia-view.php
@@ -85,7 +85,7 @@
 
                                        <?php } else { ?>
 
-                                          <form action="index.php?action=Asistencia/EditAsistencia" method="post">
+                                          <form action="index.php?view=Asistencia/EditAsistencia" method="post">
                                              <input type="hidden" name="id" value="<?php echo $asistencia->id;  ?>">
                                              <input type="hidden" name="view" value=<?php echo $_GET["view"]; ?>>
                                              <button class="dropdown-item"><i class="fa fa fa-sign-out"></i> pago</button>
