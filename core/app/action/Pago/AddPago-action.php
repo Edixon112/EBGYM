7a6ebdfa081940c4951b326605e8ecab04f38c85
Edixon <payruz16@gmail.com>
2021-06-14 11:22:13 -0500

    .

--- core/app/action/Pago/AddPago-action.php
+++ core/app/action/Pago/AddPago-action.php
@@ -30,7 +30,7 @@ $direccion = $_POST["view"];
 
 if ($direccion == 'Asistencia/ViewAsistencia') {
 
-    core::redir("./?view=Asistencia/VewAsistencia");
+    core::redir("./?view=Asistencia/ViewAsistencia");
 } else {
 
     core::redir("./?view=Pago/ViewPago");
