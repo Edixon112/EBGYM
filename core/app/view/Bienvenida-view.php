a246c9d2dd2f722765f366c45d07630720a59158
databj <benjiable21@gmail.com>
2021-07-27 23:00:00 -0500

    cositas2

--- core/app/view/Bienvenida-view.php
+++ core/app/view/Bienvenida-view.php
@@ -4,7 +4,7 @@
         <div class="card text-white border-success  mg-15">
             <div class="card-body text-success">
                 <center>
-                    <h5 class="card-title tx-success-100">Agregar entrada</h5>
+                    <h5 class="card-title tx-success-100">Agregar Entrada</h5>
                 </center>
                 <center><a href="index.php?view=Asistencia/AddAsistencia" class=" card text-black bg-wite btn btn-outline-success btn btn-primary stretched-link mg-b-10">Acceder</a> </center>
             </div>
@@ -27,7 +27,7 @@
         <div class="card text-white border-success  mg-15">
             <div class="card-body text-success">
                 <center>
-                    <h5 class="card-title tx-success-100">Ver cliente</h5>
+                    <h5 class="card-title tx-success-100">Ver clientes</h5>
                 </center>
                 <center><a href="index.php?view=Persona/ViewPersona" class=" card text-black bg-wite btn btn-outline-success btn btn-primary stretched-link mg-b-10">Acceder</a> </center>
             </div>
@@ -39,7 +39,7 @@
         <div class="card text-white border-success  mg-15">
             <div class="card-body text-success">
                 <center>
-                    <h5 class="card-title tx-success-100">Pago mensual</h5>
+                    <h5 class="card-title tx-success-100">Añadir Pago Mensual</h5>
                 </center>
                 <center><a href="index.php?view=Pago/AddPago" class=" card text-black bg-wite btn btn-outline-success btn btn-primary stretched-link mg-b-10">Acceder</a> </center>
             </div>
@@ -63,7 +63,7 @@
         <div class="card text-white bg-warning  mg-15">
             <div class="card-body text-success">
                 <center>
-                    <h5 class="card-title tx-success-100">Ver clientes en mora</h5>
+                    <h5 class="card-title tx-success-100">Ver Clientes En Mora</h5>
                 </center>
                 <center><a href="index.php?view=Pago/ViewPagoMora" class="card text-wite bg-wite btn btn-outline-warning btn-block mg-b-10">Acceder</a> </center>
             </div>
@@ -75,7 +75,7 @@
         <div class="card text-white border-success  mg-15">
             <div class="card-body text-success">
                 <center>
-                    <h5 class="card-title tx-success-100">buscar por fechas</h5>
+                    <h5 class="card-title tx-success-100">Buscar Por Fechas</h5>
                 </center>
                 <center><a href="index.php?view=Administracion/ViewIngresos" class=" card text-black bg-wite btn btn-outline-success btn btn-primary stretched-link mg-b-10">Acceder</a> </center>
             </div>
