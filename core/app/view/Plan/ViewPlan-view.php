ca9f212caab653a25104c311ebe99e9fbff6fc96
databj <benjiable21@gmail.com>
2021-08-03 19:00:26 -0500

    cambio de plan

--- core/app/view/Plan/ViewPlan-view.php
+++ core/app/view/Plan/ViewPlan-view.php
@@ -39,17 +39,7 @@
                         <td class="text-Center table-actions">
                            <div class="btn-group mg-t-5">
 
-                              <form action="index.php?view=Plan/EditPlan" method="post">
-                                 <input type="hidden" name="id" value=<?php echo $plan->id; ?>>
-                                 <input type="hidden" name="view" value=<?php echo $_GET["view"]; ?>>
-                                 <button class="btn btn-primary mg-r-5 mg-b-10" onclick="return pregunta()"><a data-toggle="tooltip" data-placement="top" title="Editar"><i class="fa fa-pencil"></i></a></button>
-                              </form>
-
-                              <form action="index.php?action=Plan/EliminarPlan" method="post">
-                                 <input type="hidden" name="id" value=<?php echo $plan->id; ?>>
-                                 <input type="hidden" name="view" value=<?php echo $_GET["view"]; ?>>
-                                 <button class="btn btn-secondary mg-r-5 mg-b-10" onclick="return pregunta()"><a data-toggle="tooltip" data-placement="top" title="Eliminar"><i class="fa fa-trash"></i></a></button>
-                              </form>
+                           
 
                            </div>
                         </td>
